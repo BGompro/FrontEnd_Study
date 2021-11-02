282c38dfe6510b6516853b042ecd653f5529ab2e
WhiteBear2021 <hyunwoo921129@gmail.com>
2021-11-02 18:31:35 +0900

    study: React 6.state
    useState()를 이용하여 버튼을 눌렀을 때 값 변경하기.
    javascript에서 할 땐 DOM 객체에 접근하여 안의 text값을 변경해주었는데,
    useState 이용하여 변경

--- react/voca/src/component/Hello.js
+++ react/voca/src/component/Hello.js
@@ -5,24 +5,22 @@
 // export default Hello;
 import World from "./World";
 import styles from "./Hello.module.css";
-export default function Hello(){
-    function showName(){
-        console.log("Mike");
-    }
+import {useState} from "react";
 
-    function showText(e){
-        console.log(e.target.value);
+export default function Hello(){
+    // let name='Mike';
+    const [name,setName]=useState('Mike');
+    function changeName(){
+        const newName=name==='Mike'?"Jane":"Mike";
+        // document.getElementById("name").innerText=name;
+        setName(newName);
     }
     return (
     <div>
-        <h1>Hello</h1>
-        <button onClick={showName}>Show name</button>
-        <button onClick={
-            ()=>{
-                console.log(30);
-            }
-        }>Show age</button>
-        <input type="text" onChange={showText}/>
+        <h1>state</h1>
+        <h2>컴포넌트의 속성값</h2>
+        <h3 id="name">이름 : {name} </h3>
+        <button onClick={changeName}>Change</button>
     </div>
     );
 }
