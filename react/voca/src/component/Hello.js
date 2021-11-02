02ca7ea5f8c1c076a354806d81cec0f8c537aa57
WhiteBear2021 <hyunwoo921129@gmail.com>
2021-11-02 12:28:07 +0900

    study: React 4.CSS
    1.인라인방식
    2.import css파일 (클래스명등 겹칠경우 overriding 되어 컴포넌트간 영향 줄 수 있음)
    3.import 객체명 from css파일을 이용하여
    객체명을 활용하여 컴포넌트에만 개별적으로 적용

--- react/voca/src/component/Hello.js
+++ react/voca/src/component/Hello.js
@@ -4,12 +4,21 @@
 
 // export default Hello;
 import World from "./World";
-
+import styles from "./Hello.module.css";
 export default function Hello(){
     return (
     <div>
-        <p>Hello</p>
+        <p style={
+        {
+            color:'#f00',
+            borderRight:'2px solid #000',
+            marginBottom: '30px',
+            opacity:0.5,
+        }
+        }
+        >Hello</p>
         <World/>
+        <div className={styles.box}>Wow~</div>
     </div>
     );
 }
