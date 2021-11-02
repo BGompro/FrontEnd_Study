4905a1797f51bb8d9d4b4a7d9fc1fab1251204f5
WhiteBear2021 <hyunwoo921129@gmail.com>
2021-11-02 23:19:59 +0900

    study: React 15 Delete
    DELETE 메서드 이용
    삭제 후 다시 랜더링해주기

--- react/voca/src/component/Word.js
+++ react/voca/src/component/Word.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-export default function Word({word}){
+export default function Word({word:w}){
+    const [word,setWord]=useState(w);
     const [isShow,setIsShow]=useState(false);
-    const [isDone,setIsDone]=useState(word.isDone);
+    const [isDone,setIsDone]=useState(w.isDone);
     function toggleShow(){
         setIsShow(!isShow);
     }
@@ -25,6 +26,22 @@ export default function Word({word}){
             }
         });
     }
+
+    function del(){
+        if(window.confirm('삭제하시겠습니까?')){
+            fetch(`http://localhost:3001/words/${word.id}`,{
+                method:"DELETE",
+            }).then(res=>{
+                if(res.ok){
+                    setWord({id:0});
+                }
+            })
+        }
+    }
+
+    if (word.id===0){
+        return null;
+    }
     return(
         <tr className={isDone?"off":""}>
             <td>
@@ -38,7 +55,7 @@ export default function Word({word}){
             </td>
             <td>
                 <button onClick={toggleShow}>뜻 {isShow?'숨기기':'보기'}</button>
-                <button className="btn_del">삭제</button>
+                <button onClick={del} className="btn_del">삭제</button>
             </td>
         </tr>
     );
