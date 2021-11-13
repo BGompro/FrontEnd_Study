fef87604b31514cbd71c95f5f2e26c6f9695a5bc
RoarinGom <hyunwoo921129@gmail.com>
2021-11-13 11:16:05 +0900

    study: Nomad 2.variable (number, string, boolean, null, undefined, array, object, function)

--- nomad/clone_Chrome/app.js
+++ nomad/clone_Chrome/app.js
@@ -1,6 +1,8 @@
 // data type
 // Number : Integer, Float 포함
 // String : text
+// Boolean : true, false(undefined, null, "", 0, NaN)
+// null, undefined
 // 변수 const : unchangeable variable, let : changeable variable
 // python : snake_case, java&javascript : camelCase recommendation
 
@@ -10,3 +12,47 @@ console.log("My name is "+myName);
 myName='Leo';
 console.log("My new name is "+myName);
 console.log("My birthday is "+myBirth);
+
+//array [] square bracket, separtated with ,
+//push('val') : add value(end),  
+//ex) days of week
+const daysOfWeek=['Mon','Tue','Wed','Thur','Fri','Sat'];
+console.log(daysOfWeek)
+daysOfWeek.push('Sun');
+console.log(daysOfWeek);
+
+//Object (entity) 
+//{} curly bracket key:value -we can know property
+//read object property -> object.key or object["key"]
+//add or update -> object.newkey=value
+const player={
+    name:"nico",
+    points:10,
+    fat:true,
+};
+console.log(player);
+console.log(player.name);
+console.log(player.points);
+console.log(player.fat);
+player.fat=false;
+console.log(player["name"]);
+console.log(player)
+
+//function : programmer is lazy -> they make function
+function sayHello(name){
+    console.log(`Hello! ${name}`)
+}
+
+sayHello("UHyeon");
+sayHello("HoYoung");
+sayHello("SungHyup");
+
+//Object with function
+const player2={
+    name:"Hyunwoo",
+    sayHello:function(otherPersonName){
+        console.log(`Hello ${otherPersonName}~ my name is ${this.name}`);
+    },
+};
+
+player2.sayHello("Lynn");
