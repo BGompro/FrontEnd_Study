825efd145a8052d9dd025dbfd8251c67175786d7
RoarinGom <hyunwoo921129@gmail.com>
2021-11-13 10:44:28 +0900

    study: Nomad js 1.setting & 2.variable
    const : unchangeable variable
    let : changeable variablel

---
+++ nomad/clone_Chrome/app.js
@@ -0,0 +1,12 @@
+// data type
+// Number : Integer, Float 포함
+// String : text
+// 변수 const : unchangeable variable, let : changeable variable
+// python : snake_case, java&javascript : camelCase recommendation
+
+let myName='Hyunwoo';
+const myBirth="11/29";
+console.log("My name is "+myName);
+myName='Leo';
+console.log("My new name is "+myName);
+console.log("My birthday is "+myBirth);
