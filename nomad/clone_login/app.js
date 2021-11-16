c600eae439315ced14933797238e94eb33e4fbf3
RoarinGom <hyunwoo921129@gmail.com>
2021-11-16 21:38:12 +0900

    study: Nomad js 7. change screen depending on username validity with localStorage

--- nomad/clone_login/app.js
+++ nomad/clone_login/app.js
@@ -5,20 +5,30 @@ const link=document.querySelector("a");
 const greeting=document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME="hidden";
-
+const USERNAME_KEY="username";
 function onLoginSubmit(event){
     event.preventDefault(); //stop default behavior(stop usually do)
-    console.log(event);
     const username=loginInput.value;
-    console.log(username);
-    localStorage.setItem("username",username);
+    localStorage.setItem(USERNAME_KEY,username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreeting(username);
+}
+
+function paintGreeting(username){
     greeting.innerText=`Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
 
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+if(savedUsername===null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    //show the greeting
+    paintGreeting(savedUsername);
+}
 
 function handleLinkClick(event){
     alert("clicked");
