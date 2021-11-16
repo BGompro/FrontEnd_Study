d6c819e2113c9f02716c4bb0155110d7eb3d706c
RoarinGom <hyunwoo921129@gmail.com>
2021-11-16 20:57:28 +0900

    study:Nomad js 3. control mouse event link with using preventDefault()

--- nomad/clone_login/app.js
+++ nomad/clone_login/app.js
@@ -1,6 +1,7 @@
 const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
 
+const link=document.querySelector("a");
 
 function onLoginSubmit(event){
     event.preventDefault(); //stop default behavior(stop usually do)
@@ -11,3 +12,10 @@ function onLoginSubmit(event){
 
 loginForm.addEventListener("submit",onLoginSubmit);
 
+
+function handleLinkClick(event){
+    alert("clicked");
+    event.preventDefault();
+    
+}
+link.addEventListener("click",handleLinkClick);
