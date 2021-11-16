6a4e7687580f23a43a0acb5f1d185006946417ff
RoarinGom <hyunwoo921129@gmail.com>
2021-11-16 21:14:21 +0900

    study: Nomad apply hidden after submitting using css class hidden
    classList.add("hidden")
    classList.remove("hidden")

--- nomad/clone_login/app.js
+++ nomad/clone_login/app.js
@@ -2,12 +2,18 @@ const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
 
 const link=document.querySelector("a");
+const greeting=document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME="hidden";
 
 function onLoginSubmit(event){
     event.preventDefault(); //stop default behavior(stop usually do)
     console.log(event);
     const username=loginInput.value;
     console.log(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
