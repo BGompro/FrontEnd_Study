2b5d81c0836c5ec46a1a348d17d15fcfd0fecf5d
RoarinGom <hyunwoo921129@gmail.com>
2021-11-16 20:49:42 +0900

    study: Nomad js 3.control submit event with using preventDefault() function

--- nomad/clone_login/app.js
+++ nomad/clone_login/app.js
@@ -1,20 +1,13 @@
+const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
-const loginButton=document.querySelector("#login-form button");
 
-// function handleLoginBtnClick(){
-//     console.log("click!!");
-//     console.dir(loginInput.value);
-// }
-loginButton.addEventListener("click",()=>{
+
+function onLoginSubmit(event){
+    event.preventDefault(); //stop default behavior(stop usually do)
+    console.log(event);
     const username=loginInput.value;
+    console.log(username);
+}
 
-    if(username===""){
-        alert("please write your name");
-    }else if(username.length>15){
-        alert("Your name is too long.")
-    }else{
-        console.log(username);
-    }
-    
-});
+loginForm.addEventListener("submit",onLoginSubmit);
 
