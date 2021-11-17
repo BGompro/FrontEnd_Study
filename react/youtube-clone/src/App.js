50259fb8224881e08d3a9366fa571fc8407c1ec4
RoarinGom <hyunwoo921129@gmail.com>
2021-11-17 21:07:15 +0900

    study: 1. youtube clone coding start

---
+++ react/youtube-clone/src/App.js
@@ -0,0 +1,29 @@
+import youtubeData from './data/youtubeData.json';
+
+function App() {
+  console.log('youtube data:', youtubeData);
+  console.log('youtube data[data]:', youtubeData['data']);
+
+  const data=youtubeData['data'][0];
+
+  console.log('data[id] :',data['id'],'-> 영상의 링크 주소');
+  console.log('data[title] :',data['title'],'-> 영상의 제목');
+  console.log('data[date] :',data['date'],'-> 업로드 날짜');
+  console.log('data[thumbnail] :',data['thumbnail'],'-> 썸네일 이미지');
+  console.log('data[description] :',data['description'],'-> 영상 설명');
+  console.log('data[category] :',data['category'],'-> 영상 분류');
+  console.log('data[viewCount] :',data['viewCount'],'-> 조회 수');
+  console.log('data[likeCount] :',data['likeCount'],'-> 좋아요 수');
+  console.log('data[channelTitle] :',data['channelTitle'],'-> 채널 제목');
+  console.log('data[channelUrl] :',data['channelUrl'],'-> 채널 url');
+  console.log('data[channelThumbnail] :',data['channelThumbnail'],'-> 채널 썸네일');
+  console.log('data[channelId] :',data['channelId'],'-> 채널 id');
+
+  
+  return (
+    <div>리액트로 데이터 불러오기
+    </div>
+  );
+}
+
+export default App;
