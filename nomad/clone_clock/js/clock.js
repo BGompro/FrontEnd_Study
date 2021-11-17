a39c26ba77f08170679851af63d7bf1419214153
RoarinGom <hyunwoo921129@gmail.com>
2021-11-17 21:59:09 +0900

    study: Nomad js 1.clock using Date

--- nomad/clone_clock/js/clock.js
+++ nomad/clone_clock/js/clock.js
@@ -2,14 +2,17 @@ const clock=document.querySelector("h2#clock");
 
 // interval : 시간 간격으로 일어나는 것
 //setInterval(callback,millisecond) : 두 개의 매개변수 받음. 1.실행할 callback함수 정의, 2.시간 간격 얼마나 두고 실행할지 밀리세컨드 단위
-function sayHello(){
-    console.log("Hello");
-}
-setInterval(sayHello,5000); //5초간격으로 sayHello함수를 실행한다.
-
 // timeout : 한번만 실행하도록 한다 특정 시간 후에
 //setTimeout(callback,millisecond) : 실행하고 싶은 함수 정의, 시간 정의
-setTimeout(sayHello,3000);
+function getClock(){
+    const date=new Date();
+    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+getClock();
+setInterval(getClock,1000); //5초간격으로 sayHello함수를 실행한다.
+
+
+
 
 
 // Date()
